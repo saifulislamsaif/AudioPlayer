ef9032508c5c3fd65127b1f16777a7d931492794
saiful islam <saifulislambaust28@gmail.com>
2022-09-28 12:15:38 +0600

    splash added

---
+++ app/src/main/java/com/cye/audioplayer/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.cye.audioplayer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_AudioPlayer )
+        setContentView(R.layout.activity_main)
+    }
+}
