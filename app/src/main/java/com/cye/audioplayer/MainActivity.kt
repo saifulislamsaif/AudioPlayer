12c9680f8369eeb133ec69fbdcf5f11a59109d45
saiful islam <saifulislambaust28@gmail.com>
2022-10-11 15:32:10 +0600

    toast test in favourite button

--- app/src/main/java/com/cye/audioplayer/MainActivity.kt
+++ app/src/main/java/com/cye/audioplayer/MainActivity.kt
@@ -2,11 +2,20 @@ package com.cye.audioplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.cye.audioplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.MusicPlayer)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.shuffleBtn.setOnClickListener {
+            Toast.makeText(this@MainActivity,"Button Clicked",Toast.LENGTH_SHORT).show()
+        }
     }
 }
