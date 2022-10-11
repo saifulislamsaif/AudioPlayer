36b96742ae4a05896c90536bd96bee7a91a57fdc
saiful islam <saifulislambaust28@gmail.com>
2022-10-11 17:11:38 +0600

    runtime permission in android

--- app/src/main/java/com/cye/audioplayer/MainActivity.kt
+++ app/src/main/java/com/cye/audioplayer/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.cye.audioplayer
 
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
 import com.cye.audioplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -29,6 +33,28 @@ class MainActivity : AppCompatActivity() {
         binding.playListBtn.setOnClickListener{
             val intent = Intent(this,PlayListActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+
+    private fun requestRuntimePermission() :Boolean{
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE), 13)
+            return false
+        }
+        return true
+    }
+
+    @RequiresApi(Build.VERSION_CODES.R)
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == 13){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Permission Granted",Toast.LENGTH_SHORT).show()
+            }
+            else
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE), 13)
         }
     }
 }
