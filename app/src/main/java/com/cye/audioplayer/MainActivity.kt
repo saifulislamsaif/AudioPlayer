e1175217176f06aab641ce993436486a0ad5083f
saiful islam <saifulislambaust28@gmail.com>
2022-10-13 10:01:42 +0600

    intially drawer added

--- app/src/main/java/com/cye/audioplayer/MainActivity.kt
+++ app/src/main/java/com/cye/audioplayer/MainActivity.kt
@@ -5,20 +5,28 @@ import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.app.ActivityCompat
 import com.cye.audioplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var toggle:ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.MusicPlayer)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        toggle = ActionBarDrawerToggle(this,binding.root,R.string.open,R.string.close)
+        binding.root.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
 //        binding.shuffleBtn.setOnClickListener {
 //            Toast.makeText(this@MainActivity,"Button Clicked",Toast.LENGTH_SHORT).show()
 //        }
@@ -56,5 +64,11 @@ class MainActivity : AppCompatActivity() {
             else
                 ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE), 13)
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item))
+            return true
+        return super.onOptionsItemSelected(item)
     }
 }
