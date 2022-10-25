d3f21cefce5ed1b0fc53a1c6381609fd773ef542
saiful islam <saifulislambaust28@gmail.com>
2022-10-25 17:45:33 +0600

    app nav margin done

--- app/src/main/java/com/cye/audioplayer/FavouriteActivity.kt
+++ app/src/main/java/com/cye/audioplayer/FavouriteActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 class FavouriteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setTheme(R.style.MusicPlayer)
+        setTheme(R.style.coolPink)
         setContentView(R.layout.activity_favourite)
     }
 }
--- app/src/main/java/com/cye/audioplayer/PlayListActivity.kt
+++ app/src/main/java/com/cye/audioplayer/PlayListActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 class PlayListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setTheme(R.style.MusicPlayer)
+        setTheme(R.style.coolPink)
         setContentView(R.layout.activity_play_list)
     }
 }
