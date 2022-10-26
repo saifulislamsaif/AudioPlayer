361af9dc0b9cc73fc623f07edf7d5ba223fb677b
saiful islam <saifulislambaust28@gmail.com>
2022-10-26 12:53:47 +0600

    MusicAdapter added

--- app/src/main/java/com/cye/audioplayer/MainActivity.kt
+++ app/src/main/java/com/cye/audioplayer/MainActivity.kt
@@ -10,6 +10,7 @@ import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.app.ActivityCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.cye.audioplayer.databinding.ActivityMainBinding
 import kotlin.system.exitProcess
 
@@ -17,17 +18,12 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var toggle:ActionBarDrawerToggle
+    private lateinit var musicAdapter: MusicAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setTheme(R.style.MusicPlayer)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        toggle = ActionBarDrawerToggle(this,binding.root,R.string.open,R.string.close)
-        binding.root.addDrawerListener(toggle)
-        toggle.syncState()
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        innitializeLayout()
 //        binding.shuffleBtn.setOnClickListener {
 //            Toast.makeText(this@MainActivity,"Button Clicked",Toast.LENGTH_SHORT).show()
 //        }
@@ -80,5 +76,27 @@ class MainActivity : AppCompatActivity() {
         if (toggle.onOptionsItemSelected(item))
             return true
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun innitializeLayout(){
+        setTheme(R.style.MusicPlayer)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        toggle = ActionBarDrawerToggle(this,binding.root,R.string.open,R.string.close)
+        binding.root.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        val musicList = ArrayList<String>()
+        musicList.add("First Song")
+        musicList.add("2nd Song")
+        musicList.add("3rd Song")
+        musicList.add("4th Song")
+        musicList.add("5th Song")
+        binding.musicRV.setHasFixedSize(true)
+        binding.musicRV.setItemViewCacheSize(13)
+        binding.musicRV.layoutManager= LinearLayoutManager(this)
+        musicAdapter =MusicAdapter(this,musicList)
+        binding.musicRV.adapter =musicAdapter
+        binding.totalSongs.text="Total Songs : "+musicAdapter.itemCount
     }
 }
