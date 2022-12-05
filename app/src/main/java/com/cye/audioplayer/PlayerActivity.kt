3a1bf25a0b4bf65f4cbafaf84d7ec374d4032458
saiful islam <saifulislambaust28@gmail.com>
2022-12-05 17:49:29 +0600

    playing song and pause song with album

--- app/src/main/java/com/cye/audioplayer/PlayerActivity.kt
+++ app/src/main/java/com/cye/audioplayer/PlayerActivity.kt
@@ -1,33 +1,81 @@
 package com.cye.audioplayer
 
+import android.annotation.SuppressLint
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.cye.audioplayer.databinding.ActivityPlayerBinding
 
 class PlayerActivity : AppCompatActivity() {
-    companion object{
-        lateinit var musicListPlA:ArrayList<Music>
-        var songPosition:Int = 0
-        var mediaPlayer:MediaPlayer? = null
+    companion object {
+        lateinit var musicListPlA: ArrayList<Music>
+        var songPosition: Int = 0
+        var mediaPlayer: MediaPlayer? = null
+        var isPlaying: Boolean = false
+         @SuppressLint("StaticFieldLeak")
+         lateinit var binding: ActivityPlayerBinding
     }
-    private lateinit var binding:ActivityPlayerBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.MusicPlayer)
-        setContentView(R.layout.activity_player)
-        songPosition = intent.getIntExtra("index",0)
-        when(intent.getStringExtra("class") )
-        {
-            "MusicAdapter"->{
+        binding = ActivityPlayerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initializeLayout()
+        binding.playPauseBtnPA.setOnClickListener {
+            if (isPlaying) pauseMusic()
+            else
+                playMusic()
+        }
+    }
+
+    private fun setLayout() {
+        Glide.with(applicationContext)
+            .load(musicListPlA[songPosition].artUri)
+            .apply(RequestOptions().placeholder(R.drawable.music_player_icon_slash_screen).centerCrop())
+            .into(binding.songImgPA)
+        binding.songNamePA.text = musicListPlA[songPosition].title
+    }
+
+    private fun createMediaPlayer() {
+        try {
+            if (mediaPlayer == null) mediaPlayer = MediaPlayer()
+            mediaPlayer!!.reset()
+            mediaPlayer!!.setDataSource(musicListPlA[songPosition].path)
+            mediaPlayer!!.prepare()
+            mediaPlayer!!.start()
+            isPlaying = true
+            binding.playPauseBtnPA.setIconResource(R.drawable.pause_icon)
+        } catch (e: java.lang.Exception) {
+            return
+        }
+
+    }
+
+    private fun initializeLayout() {
+        songPosition = intent.getIntExtra("index", 0)
+        when (intent.getStringExtra("class")) {
+            "MusicAdapter" -> {
                 musicListPlA = ArrayList()
                 musicListPlA.addAll(MainActivity.MusicListMA)
-                if (mediaPlayer==null) mediaPlayer = MediaPlayer()
-                mediaPlayer!!.reset()
-                mediaPlayer!!.setDataSource(musicListPlA[songPosition].path)
-                mediaPlayer!!.prepare()
-                mediaPlayer!!.start()
+                setLayout()
+                createMediaPlayer()
             }
         }
+    }
+
+    private fun playMusic() {
+        binding.playPauseBtnPA.setIconResource(R.drawable.pause_icon)
+        isPlaying = true
+        mediaPlayer!!.start()
+    }
+
+    private fun pauseMusic() {
+        binding.playPauseBtnPA.setIconResource(R.drawable.play_icon)
+        isPlaying = false
+        mediaPlayer!!.pause()
     }
 }
