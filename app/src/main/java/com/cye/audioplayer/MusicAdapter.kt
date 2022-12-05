361ea438cb77828a97bd25fa45e36379c7d0d4c2
saiful islam <saifulislambaust28@gmail.com>
2022-12-05 16:36:16 +0600

    duration of songs

--- app/src/main/java/com/cye/audioplayer/MusicAdapter.kt
+++ app/src/main/java/com/cye/audioplayer/MusicAdapter.kt
@@ -3,8 +3,10 @@ package com.cye.audioplayer
 import android.annotation.SuppressLint
 import android.app.DownloadManager.Request
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
@@ -18,7 +20,7 @@ class MusicAdapter(private val context: Context, private var musicList: ArrayLis
         val album = binding.songAlbumMV
         val image = binding.imageMV
         val duration = binding.songDuration
-
+        val root = binding.root
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyHolder {
@@ -29,12 +31,17 @@ class MusicAdapter(private val context: Context, private var musicList: ArrayLis
     override fun onBindViewHolder(holder: MyHolder, position: Int) {
         holder.title.text = musicList[position].title
         holder.album.text = musicList[position].album
-        holder.duration.text = musicList[position].duration.toString()
+        holder.duration.text = formatDuration(musicList[position].duration)
         Glide.with(context)
             .load(musicList[position])
             .apply(RequestOptions().placeholder(R.drawable.music_player_icon_slash_screen).centerCrop())
             .into(holder.image)
+        holder.root.setOnClickListener{
+            val intent = Intent(context,PlayListActivity::class.java)
+            ContextCompat.startActivity(context,intent,null)
+        }
     }
+
 
     override fun getItemCount(): Int {
         return musicList.size
